285e810e5eb6d694757f1dd72dcca564ee508517
vijay paul <vjpaul03@gmail.com>
2026-04-29 02:57:14 +0530

    final

--- mojito-gsap/src/components/Navbar.jsx
+++ mojito-gsap/src/components/Navbar.jsx
@@ -1,48 +1,42 @@
-import React from 'react';
-import { navLinks } from '../../constants';
-import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import { useGSAP } from '@gsap/react'
+
+import { navLinks } from '../../constants/index.js'
 
 const Navbar = () => {
  useGSAP(() => {
-  const nav = document.querySelector('nav');
-
-  const navTween = gsap.timeline({
-    scrollTrigger: {
-      trigger: 'nav',
-      start: 'bottom top',
-    }
-  });
-
-  navTween.fromTo('nav',
-    { backgroundColor: 'transparent' },
-    {
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      backdropFilter: 'blur(10px)',
-      duration: 1,
-      ease: 'power1.inOut'
-    }
-  );
-});
-
-  return (
-    <nav>
-      <div>
-        <a href="#home" className="flex items-center gap-2">
-          <img src="/images/logo.png" alt="logo"/>
-        <p>Exotic Drinks</p>
-        </a>
-        <ul>
-            {navLinks.map((link) =>(
-                <li key={link.id}>
-                <a href={`#${link.id}`}>{link.title}</a>
-
-                </li> 
-            ))}
-            </ul>
-      </div>
-    </nav>
-  )
+	const navTween = gsap.timeline({
+	 scrollTrigger: {
+		trigger: 'nav',
+		start: 'bottom top'
+	 }
+	});
+	
+	navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+	 backgroundColor: '#00000050',
+	 backgroundFilter: 'blur(10px)',
+	 duration: 1,
+	 ease: 'power1.inOut'
+	});
+ })
+ 
+ return (
+	<nav>
+	 <div>
+		<a href="#home" className="flex items-center gap-2">
+		 <img src="/images/logo.png" alt="logo" />
+		 <p>Velvet Pour</p>
+		</a>
+		
+		<ul>
+		 {navLinks.map((link) => (
+			<li key={link.id}>
+			 <a href={`#${link.id}`}>{link.title}</a>
+			</li>
+		 ))}
+		</ul>
+	 </div>
+	</nav>
+ )
 }
-
 export default Navbar
